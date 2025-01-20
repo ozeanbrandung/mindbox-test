f2a429e385324df2395d2bc825267afe1fe856c2
ozeanbrandung <natalia.v.molchanova@yandex.ru>
2025-01-21 02:45:06 +0400

    add initial todos layout and styles

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,8 +1,38 @@
+import styles from './todo-list.module.scss';
+
+const todos = [
+    { id: 1, text: 'Learn React', isCompleted: false },
+    { id: 2, text: 'Learn Vue', isCompleted: false },
+    { id: 3, text: 'Learn Angular', isCompleted: false },
+]
+
 export function TodoList() {
 
   return (
-   <div>
-        Todo list
-   </div>
+   <section className={styles.todoList}>
+        <h1 className={styles.title}>todos</h1>
+
+        <div className={styles.border}>
+            <input className={styles.input} type="text" placeholder="What needs to be done?" />
+            
+            <ul className={styles.list}>
+                {todos.map((todo) => (
+                 <li className={styles.item}>
+                    <input type="checkbox" checked={todo.isCompleted} />
+                    <span>{todo.text}</span>
+                </li>
+             ))}
+            </ul>
+            <footer className={styles.footer}>
+                <span className={styles.left}>0 left</span>
+                <ul className={styles.filters}>
+                    <li className={styles.filter} tabIndex={0}>All</li>
+                    <li className={styles.filter} tabIndex={0}>Active</li>
+                    <li className={styles.filter} tabIndex={0}>Completed</li>
+                </ul>
+                <button className={styles.clear}>Clear Completed</button>
+            </footer>
+        </div>
+   </section>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['**/*.scss'], // Include all .scss files
+  },
+  css: {
+    modules: {
+      // Enable CSS Modules for all .scss files
+      localsConvention: 'camelCaseOnly',
+    },
+  },
 })
