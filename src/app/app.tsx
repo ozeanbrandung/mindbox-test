5648cc960d78dc1e8ffdab32d068667c548253f5
ozeanbrandung <natalia.v.molchanova@yandex.ru>
2025-01-21 01:53:10 +0400

    add tanstack query; query client; add api instance

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 //import { useState } from 'react'
 
+import { TodoList } from "../modules/todo-list/todo-list"
+
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
    <div>
-hi
+    <TodoList />
    </div>
   )
 }
---
+++ src/modules/todo-list/todo-list.tsx
@@ -0,0 +1,8 @@
+export function TodoList() {
+
+  return (
+   <div>
+        Todo list
+   </div>
+  );
+}
