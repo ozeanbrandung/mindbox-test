a8d3b84c06e82f7f8e458e1042946f547125e80d
ozeanbrandung <natalia.v.molchanova@yandex.ru>
2025-01-21 15:47:55 +0400

    add build options to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,11 @@ export default defineConfig({
       localsConvention: 'camelCaseOnly',
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      }
+    }
+  }
 })
